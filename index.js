d86aff6a3a4fcb9c1ff9b4f55a47dd4b1c5b4eea
Richard van der Dys <richard.vanderdys@gmail.com>
2016-03-25 17:29:43 -0500

    test

--- index.js
+++ index.js
@@ -12,11 +12,17 @@ var bot = new Roomba({
 
 bot.once('ready', function() {
   console.log('bot is ready');
+  console.log('stopping spin');
+  bot.send({
+    cmd: 'DRIVE',
+    data: [0, -1]
+  });
   bot.send({
     cmd: 'SONG',
     // data: [1, 6, 62, 30, 64, 30, 65, 160, 64, 50, 60, 50, 53, 120]
     data: [1, 16, 76, 16, 76, 16, 76, 32, 76, 16, 76, 16, 76, 32, 76, 16, 79, 16,
-      72, 16, 74, 16, 76, 32, 77, 16, 77, 16, 77, 16, 77, 32, 77, 16]
+      72, 16, 74, 16, 76, 32, 77, 16, 77, 16, 77, 16, 77, 32, 77, 16
+    ]
   });
   bot.send({
     cmd: 'PLAY',
@@ -24,10 +30,13 @@ bot.once('ready', function() {
   });
 });
 
-bot.on('sense', function (sensors) {
-    if (sensors.bump.right || sensors.bump.left) {
-        console.log('bump detected');
-        // stop spinning
-        bot.send({ cmd: 'DRIVE', data: [0, -1] });
-    }
+bot.on('sense', function(sensors) {
+  if (sensors.bump.right || sensors.bump.left) {
+    console.log('bump detected');
+    // stop spinning
+    bot.send({
+      cmd: 'DRIVE',
+      data: [0, -1]
+    });
+  }
 });
