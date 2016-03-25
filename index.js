3eb79ac2a1d9ad1932f8565794e70a12c1bf8fe8
Richard van der Dys <richard.vanderdys@gmail.com>
2016-03-25 17:08:36 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var Roomba = require('roomba').Roomba;
+
+var bot = new Roomba({
+  sp: {
+    path: '/dev/ttyAMA0',
+    options: {
+      baudrate: 57600
+    }
+  }
+});
+
+bot.once('ready', function() {
+  bot.send({
+    cmd: 'SONG',
+    data: [1, 6, 62, 30, 64, 30, 65, 160, 64, 50, 60, 50, 53, 120]
+  });
+  bot.send({
+    cmd: 'PLAY',
+    data: [1]
+  });
+});
