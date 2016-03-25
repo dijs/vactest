1acd18ea2319f84e0f2f136e5cb750b452cb698b
Richard van der Dys <richard.vanderdys@gmail.com>
2016-03-25 17:25:56 -0500

    test

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ var bot = new Roomba({
     options: {
       baudrate: 57600
     }
-  }
+  },
+  update_freq: 500
 });
 
 bot.once('ready', function() {
+  console.log('bot is ready');
   bot.send({
     cmd: 'SONG',
     // data: [1, 6, 62, 30, 64, 30, 65, 160, 64, 50, 60, 50, 53, 120]
@@ -20,4 +22,12 @@ bot.once('ready', function() {
     cmd: 'PLAY',
     data: [1]
   });
+});
+
+bot.on('sense', function (sensors) {
+    if (sensors.bump.right || sensors.bump.left) {
+        console.log('bump detected');
+        // stop spinning
+        bot.send({ cmd: 'DRIVE', data: [0, -1] });
+    }
 });
