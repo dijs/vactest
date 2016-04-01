8f36ce2726a49e63a26436abbb5838c077685b27
Richard van der Dys <richard.vanderdys@gmail.com>
2016-04-01 07:47:53 -0500

    Fixed driving test

--- index.js
+++ index.js
@@ -1,56 +1,31 @@
 var robot = require('create-oi');
 
+var isOSX = process.argv.length > 1 &&
+  process.argv[2].toLowerCase() === '--osx';
+var LINUX_PORT = '/dev/ttyUSB0';
+var OSX_PORT = '/dev/tty.usbserial-DA01NNR3';
+var SPEED = 100; // 100 mm/s
+
 robot.init({
-  // serialport: '/dev/ttyUSB0'
-  serialport: '/dev/ttyAMA0'
+  serialport: isOSX ? OSX_PORT : LINUX_PORT,
+  version: 2
+});
+
+function testDrive(bot) {
+  var drive = bot.drive;
+  var wait = bot.wait;
+
+  drive(SPEED, 0)
+    .then(wait(1000))
+    .then(drive(0, 0));
+}
+
+robot.on('ready', function() {
+  console.log('ready');
+  testDrive(this);
 });
 
 robot.on('bump', function(bumpEvent) {
-  console.log('bumped', bumpEvent.direction);
+  console.log('bumped', bumpEvent.which);
+  testDrive(this);
 });
-
-
-//
-// var Roomba = require('roomba').Roomba;
-//
-// var bot = new Roomba({
-//   sp: {
-//     path: '/dev/ttyUSB0',
-//     options: {
-//       baudrate: 57600
-//     }
-//   },
-//   update_freq: 200
-// });
-//
-// function test() {
-//   // console.log('stopping spin');
-//   // bot.send({
-//   //   cmd: 'DRIVE',
-//   //   data: [0, -1]
-//   // });
-//   console.log('sending song');
-//   bot.send({
-//     cmd: 'SONG',
-//     data: [1, 6, 62, 30, 64, 30, 65, 160, 64, 50, 60, 50, 53, 120]
-//   });
-//   console.log('sending play');
-//   bot.send({
-//     cmd: 'PLAY',
-//     data: [1]
-//   });
-// }
-//
-// bot.once('ready', function() {
-//   console.log('bot is ready');
-//   // setTimeout(test, 2000);
-//   //
-// });
-//
-// bot.on('sense', function(sensors) {
-//   console.log('sensing something');
-//   if (sensors.bump.right || sensors.bump.left) {
-//     console.log('bump detected');
-//     // test();
-//   }
-// });
